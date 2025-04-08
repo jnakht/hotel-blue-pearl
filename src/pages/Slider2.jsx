ffa849036d47f3fe8b720bf7f57811be1f630ce4
jnakht <jisan.cse.cu@gmail.com>
2025-04-08 16:55:02 +0600

    all four slides of the slider added

--- src/pages/Slider2.jsx
+++ src/pages/Slider2.jsx
@@ -1,8 +1,14 @@
 
 const Slider2 = () => {
     return (
-        <div className="bg-background text-mText h-full w-full border-2 border-purple-800">
-            This is slider 2
+        <div className='h-full  '>
+            <div className="w-full bg-[url('https://i.ibb.co.com/MyHqgQnB/pexels-airamdphoto-15883109.jpg')] bg-cover bg-center  bg-no-repeat h-full  ">
+                <div className='w-[50%]  text-mText  pt-[100px] pl-[50px]'>
+                    <h3 className='text-mText text-5xl font-medium'>Elegant Rooms & Suites</h3>
+                    <p className='text-mText2 text-2xl font-light '>Relax in style in our beautifully designed rooms and suites, equipped with modern comforts and luxurious touches. Whether you're here for business or leisure, our accommodations promise a restful and rejuvenating stay.</p>
+                </div>
+            </div>
+
         </div>
     );
 };
--- src/pages/Slider3.jsx
+++ src/pages/Slider3.jsx
@@ -1,8 +1,14 @@
 
 const Slider3 = () => {
     return (
-        <div className="bg-background text-mText h-full w-full border-2 border-purple-800">
-            This is slider 3
+        <div className='h-full  '>
+            <div className="w-full bg-[url('https://i.ibb.co.com/MyHqgQnB/pexels-airamdphoto-15883109.jpg')] bg-cover bg-center  bg-no-repeat h-full  ">
+                <div className='w-[50%]  text-mText  pt-[100px] pl-[50px]'>
+                    <h3 className='text-mText text-5xl font-medium'>Unwind with Our Amenities</h3>
+                    <p className='text-mText2 text-2xl font-light '>Take a dip in the rooftop pool, relax at our luxury spa, or enjoy a workout in the state-of-the-art fitness center. Our wide range of amenities is designed to make your stay both indulgent and memorable.</p>
+                </div>
+            </div>
+
         </div>
     );
 };
