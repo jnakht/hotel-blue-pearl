aedd605dffbba212ac54b2af1f8b7049e90259b6
jnakht <jisan.cse.cu@gmail.com>
2025-04-10 11:27:52 +0600

    if id is even, homepage cards are made flex-row-reverse

--- src/components/SingleHomeCard.jsx
+++ src/components/SingleHomeCard.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const SingleHomeCard = ({card}) => {
-    const {image, title, description} = card;
+    const {image, title, description, id} = card;
+    const idNumber = parseInt(id);
+    // console.log(idNumber)
     return (
-        <div className="card lg:card-side h-[527px] bg-background2 shadow-sm">
+        <div className={`card lg:card-side h-[527px] bg-background2 shadow-sm flex flex-col ${idNumber % 2 === 0 && 'lg:flex-row-reverse'}`}>
         <figure className='w-[40%]'>
           <img
             
