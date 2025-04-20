6cb81c965e739bd89b20b51fc6a07f604f436bde
jnakht <jisan.cse.cu@gmail.com>
2025-04-20 21:34:17 +0600

    register page basic stylin done

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -35,32 +35,32 @@ const Register = () => {
                                 <div className="  ">
                                     <div className="flex gap-2 flex-col mb-3">
                                     <h1 className="text-3xl font-bold ">Welcome Hotel Blue Pearl</h1>
-                                    <p className="text-sm font-light text-[#8A8A93]">Create a new account</p>
+                                    <p className="text-sm font-light text-[#828283]">Create a new account</p>
                                     </div>
                                     <button className="flex btn text-xl w-full bg-background text-mText border-none items-center mb-8 mt-10"><FcGoogle className="text-3xl" /> Continue With Google</button>
-                                    <p className="text-gray-400 text-xl mb-8 ">---------------------or----------------------</p>
+                                    <p className="text-[#828283] text-xl mb-8 ">---------------------or----------------------</p>
                                 </div>
                                 <label className="fieldset-label text-mText">Name</label>
-                                <input  {...register("name", { required: true })} type="text" className="input bg-background2" placeholder="What should i call you?" />
+                                <input  {...register("name", { required: true })} type="text" className="input border border-[#828283] w-full bg-background2" placeholder="What should i call you?" />
                                 {errors.name && <span className='text-red-400'>This field is required</span>}
 
 
                                 <label className="fieldset-label text-mText">Photo URL</label>
-                                <input {...register("photoURL", { required: true })} type="text" className="input bg-background2" placeholder="Photo URL" />
+                                <input {...register("photoURL", { required: true })} type="text" className="input border border-[#828283] w-full bg-background2" placeholder="Photo URL" />
                                 {errors.name && <span className='text-red-400'>This field is required</span>}
 
                                 <label className="fieldset-label text-mText">Email</label>
-                                <input {...register("email", { required: true })} type="email" className="input bg-background2" placeholder="Your email" />
+                                <input {...register("email", { required: true })} type="email" className="input border border-[#828283] w-full bg-background2" placeholder="Your email" />
                                 {errors.email && <span className='text-red-400'>This field is required</span>}
 
 
                                 <label className="fieldset-label text-mText">Password</label>
-                                <input {...register("password", { required: true })} type="password" className="input bg-background2" placeholder="Password" />
+                                <input {...register("password", { required: true })} type="password" className="input border border-[#828283] w-full bg-background2" placeholder="Password" />
                                 {errors.password && <span className='text-red-400'>This field is required</span>}
 
 
                                 <div><a className="link link-hover">Forgot password?</a></div>
-                                <button className="btn btn-neutral mt-4">Sign Up</button>
+                                <button className="btn bg-[#828283] border-none mt-4">Sign Up</button>
                             </fieldset>
                         </div>
                     </form>
