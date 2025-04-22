2ccfdf87690d636f5f4f71008093ab2fcc322a90
jnakht <jisan.cse.cu@gmail.com>
2025-04-22 14:21:51 +0600

    basic modal added on BookNow button

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -2,22 +2,38 @@ import '../utility/HeroCard.css'
 import { GoArrowUpRight } from "react-icons/go";
 
 const HeroCard = () => {
-    return (
-        <div  className="card lg:card-side h-[640px] bg-background2 shadow-sm mt-[76px]">
-        <figure className="bg-[#C4B5FD] w-[35%] flex justify-center items-center floating-container">
-          <img
-            className='hero-card-img floating-laptop'
-            src="https://cdn.getmerlin.in/cms/image_ce68ee704e.webp"
-            alt="Album" />
-        </figure>
-        <div className="max-w-[45%] flex flex-col  mx-auto mt-30">
-          <h2 className="text-5xl mb-4 font-medium">Stay Connected, Wherever You Are</h2>
-          <p className='text-2xl text-[#A1A1AA] font-light '>Access all the comforts of Hotel Blue Pearl from any device. Browse room options, manage your bookings, and explore exclusive offers — all with a seamless experience across your phone, tablet, and computer.</p>
-          <button className='btn max-w-[30%] mt-5 text-lg font-semibold'>Book Now <GoArrowUpRight className='text-3xl font-bold' /></button>
-        </div>
-       
+  
+  return (
+    <div className="card lg:card-side h-[640px] bg-background2 shadow-sm mt-[76px]">
+      <figure className="bg-[#C4B5FD] w-[35%] flex justify-center items-center floating-container">
+        <img
+          className='hero-card-img floating-laptop'
+          src="https://cdn.getmerlin.in/cms/image_ce68ee704e.webp"
+          alt="Album" />
+      </figure>
+      <div className="max-w-[45%] flex flex-col  mx-auto mt-30">
+        <h2 className="text-5xl mb-4 font-medium">Stay Connected, Wherever You Are</h2>
+        <p className='text-2xl text-[#A1A1AA] font-light '>Access all the comforts of Hotel Blue Pearl from any device. Browse room options, manage your bookings, and explore exclusive offers — all with a seamless experience across your phone, tablet, and computer.</p>
+        <button className='btn max-w-[30%] mt-5 text-lg font-semibold' onClick={() => document.getElementById('my_modal_3').showModal()}>Book Now <GoArrowUpRight className='text-3xl font-bold' /></button>
+
+
+        {/* dropdown modal */}
+        {/* You can open the modal using document.getElementById('ID').showModal() method */}
+        {/* <button className="btn" onClick={() => document.getElementById('my_modal_3').showModal()}>open modal</button> */}
+        <dialog id="my_modal_3" className="modal bg-background">
+          <div className="modal-box bg-background">
+            {/* <form   method="dialog bg-background"> */}
+              {/* if there is a button in form, it will close the modal */}
+              <button onClick={() => document.getElementById('my_modal_3').close()} className="btn btn-sm btn-circle btn-ghost absolute right-2 top-2">✕</button>
+            {/* </form> */}
+            <h3 className="font-bold text-lg">Hello!</h3>
+            <p className="py-4">Press ESC key or click on ✕ button to close</p>
+          </div>
+        </dialog>
       </div>
-    );
+
+    </div>
+  );
 };
 
 export default HeroCard;
