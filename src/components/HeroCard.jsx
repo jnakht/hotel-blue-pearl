cceaeeb2c98fc3798aa242f38208253826b9509f
jnakht <jisan.cse.cu@gmail.com>
2025-04-22 17:42:05 +0600

    the modal is now responsive

--- src/components/HeroCard.jsx
+++ src/components/HeroCard.jsx
@@ -14,7 +14,7 @@ const HeroCard = () => {
       <div className="lg:max-w-[45%] flex flex-col  mx-auto mt-8 lg:mt-30 pl-4 lg:pl-0">
         <h2 className="text-2xl md:text-3xl lg:text-5xl mb-4 font-medium">Stay Connected, Wherever You Are</h2>
         <p className='text-2xl text-[#A1A1AA] font-light '>Access all the comforts of Hotel Blue Pearl from any device. Browse room options, manage your bookings, and explore exclusive offers — all with a seamless experience across your phone, tablet, and computer.</p>
-        <button className='btn max-w-[30%] mt-5 text-lg font-semibold' onClick={() => document.getElementById('my_modal_3').showModal()}>Book Now <GoArrowUpRight className='text-3xl font-bold' /></button>
+        <button className='btn max-w-[60%] md:max-w-[60%] lg:max-w-[50%] mt-5 text-base  lg:text-lg font-semibold' onClick={() => document.getElementById('my_modal_3').showModal()}>Book Now <GoArrowUpRight className='text-3xl font-bold' /></button>
 
 
         {/* dropdown modal */}
